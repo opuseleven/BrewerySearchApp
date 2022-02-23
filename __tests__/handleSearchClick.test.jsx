6c9bffd591e07c16a357ec5e3ea3ec825770d816
Cody Clark <opuseleven@protonmail.com>
2022-02-23 17:31:39 -0600

    Added test for handleSearchClick function

---
+++ __tests__/handleSearchClick.test.jsx
@@ -0,0 +1,16 @@
+import { handleSearchClick } from '../services';
+
+describe('handleSearchClick function', () => {
+
+  let breweries = [];
+  function setBreweries(newArr) {
+    breweries = newArr;
+  }
+  let searchTerm = "Nashville";
+
+  it('Functions to search database and setBreweries array', async () => {
+    handleSearchClick(searchTerm, setBreweries);
+    await new Promise(r => setTimeout(r, 3000));
+    expect(breweries.length).toBeGreaterThan(1);
+  })
+})
