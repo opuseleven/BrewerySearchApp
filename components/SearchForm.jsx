8c658b072902cc7e7b5d208d3c7914108226c4b1
Cody Clark <opuseleven@protonmail.com>
2022-02-23 15:28:17 -0600

    Added SearchForm component

---
+++ components/SearchForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function SearchForm({ setSearchTerm }) {
+
+  function useField(type) {
+    const [value, setValue] = useState('');
+    function onChange(event) {
+      setValue(event.target.value);
+    }
+    return {
+      type,
+      value,
+      onChange
+    }
+  }
+
+  const searchInput = useField('text');
+
+  return (
+    <div>
+      <form>
+        <input {...searchInput} />
+        <button onClick={setSearchTerm(searchInput.value)}>Submit</button>
+      </form>
+    </div>
+  )
+}
+export { SearchForm };
