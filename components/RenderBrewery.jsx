9f926c7ba9e905304c24b780d3fc97f118f7f68b
Cody Clark <opuseleven@protonmail.com>
2022-02-23 16:40:49 -0600

    Fixed in case no brewery is passed to component.

--- components/RenderBrewery.jsx
+++ components/RenderBrewery.jsx
@@ -1,14 +1,18 @@
-import { Brewery } from '../types';
-
 function RenderBrewery({ brewery }) {
 
   return (
-    <div key={brewery.obdb_id}>
-      <h3>{brewery.name}</h3>
-      <p>Brewery Type: {brewery.brewery_type}</p>
-      <p>{brewery.street}</p>
-      <p>{brewery.city}, {brewery.state}</p>
-      <p><a href={brewery.website_url}>{brewery.website_url}</a></p>
+    <div>
+    {
+      brewery && (
+        <div key={brewery.obdb_id}>
+          <h3>{brewery.name}</h3>
+          <p>Brewery Type: {brewery.brewery_type}</p>
+          <p>{brewery.street}</p>
+          <p>{brewery.city}, {brewery.state}</p>
+          <p><a href={brewery.website_url}>{brewery.website_url}</a></p>
+        </div>
+      )
+    }
     </div>
   )
 }
