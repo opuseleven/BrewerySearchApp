71a1b23aba7a28198ab91790b4eb439c6021c8b0
Cody Clark <opuseleven@protonmail.com>
2022-02-25 14:16:48 -0600

    Expanded on props a bit.

--- components/RenderBrewery.jsx
+++ components/RenderBrewery.jsx
@@ -2,16 +2,24 @@ import styles from '../styles/Components.module.css';
 
 function RenderBrewery({ brewery }) {
 
+  const key = brewery.obdb_id;
+  const name = brewery.name;
+  const type = brewery.brewery_type;
+  const street = brewery.street;
+  const city = brewery.city;
+  const state = brewery.state;
+  const url = brewery.website_url;
+
   return (
     <div>
     {
       brewery && (
-        <div className={styles.renderbrewerycontainer} key={brewery.obdb_id}>
-          <h3>{brewery.name}</h3>
-          <p>Brewery Type: {brewery.brewery_type}</p>
-          <p>{brewery.street}</p>
-          <p>{brewery.city}, {brewery.state}</p>
-          <p><a href={brewery.website_url}>{brewery.website_url}</a></p>
+        <div className={styles.renderbrewerycontainer} key={key}>
+          <h3>{name}</h3>
+          <p>Brewery Type: {type}</p>
+          <p>{street}</p>
+          <p>{city}, {state}</p>
+          <p><a href={url}>{url}</a></p>
         </div>
       )
     }
