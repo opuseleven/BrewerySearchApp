325ebaefa17baf1a8a8e87ea8539ddfb713ab0a7
Cody Clark <opuseleven@protonmail.com>
2022-02-25 15:10:16 -0600

    Removed unnecessary old code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,11 @@ import styles from '../styles/Home.module.css'
 import { Brewery } from '../types';
 import { useState, useEffect } from 'react';
 import { SearchForm, RenderBrewery } from '../components';
-import { handleSearchClick, useField } from '../services';
 
 const Home: NextPage = () => {
 
   const [breweries, setBreweries] = useState<Brewery[]>([]);
   const [displayedBreweries, setDisplayedBreweries] = useState<Brewery[]>([]);
-
-  const searchInput = useField('text');
-
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    handleSearchClick(searchInput.value, setBreweries);
-    setDisplayedBreweries(breweries);
-  }
 
   useEffect(() => {
     setDisplayedBreweries(breweries);
