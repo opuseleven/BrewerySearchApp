0b602255cc0a2e372c149bd044a52ec24d88b939
Cody Clark <opuseleven@protonmail.com>
2022-02-25 21:00:20 -0600

    Added tests for StateSelector component

---
+++ __tests__/StateSelector.test.jsx
@@ -0,0 +1,24 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { StateSelector } from '../components';
+
+describe('StateSelector component', () => {
+
+  let state = '--select state--';
+  function setState(s) {
+    state = s;
+  }
+
+  it('Renders', () => {
+    render(<StateSelector />);
+    const options = screen.getAllByRole('option');
+    expect(options[0]).toHaveTextContent('--select state--');
+  })
+
+  it('Changes state value', () => {
+    render(<StateSelector setStateFilter={setState} />);
+    const selector = screen.getByRole('combobox');
+    fireEvent.change(selector, {target: {value: "alaska"}});
+    expect(state).toBe('alaska');
+  })
+})
