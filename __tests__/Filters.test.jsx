45ad5166835edd635bc1cce158e385c2a9b65e0b
Cody Clark <opuseleven@protonmail.com>
2022-02-25 21:01:26 -0600

    Added tests for Filters component

---
+++ __tests__/Filters.test.jsx
@@ -0,0 +1,36 @@
+import { Filters } from '../components';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Filters component', () => {
+
+  let state = "--select state--";
+  let type = "--select type--";
+
+  function setState(s) {
+    state = s;
+  }
+  function setType(s) {
+    type = s;
+  }
+
+  it('Renders', () => {
+    render(<Filters setStateFilter={setState} setTypeFilter={setType} />);
+    const boxes = screen.getAllByRole('combobox');
+    expect(boxes[0]).toHaveTextContent('--select state--');
+    expect(boxes[1]).toHaveTextContent('--select type--');
+  })
+
+  it('Changes both state and type variables', () => {
+    render(<Filters setStateFilter={setState} setTypeFilter={setType} />);
+    const boxes = screen.getAllByRole('combobox');
+    fireEvent.change(boxes[0], {target: {value: "colorado"}});
+    expect(boxes[0].value).toBe('colorado');
+    expect(state).toBe('colorado');
+    fireEvent.change(boxes[1], {target: {value: "microbrewery"}});
+    expect(boxes[0].value).toBe('colorado');
+    expect(boxes[1].value).toBe('microbrewery');
+    expect(type).toBe('microbrewery');
+  })
+
+})
