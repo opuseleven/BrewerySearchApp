1aa195b91423b68ab58fdf3dd02af7e39b24a5c9
Cody Clark <opuseleven@protonmail.com>
2022-02-26 14:24:40 -0600

    Fixed test

--- __tests__/Filters.test.jsx
+++ __tests__/Filters.test.jsx
@@ -4,33 +4,41 @@ import '@testing-library/jest-dom';
 
 describe('Filters component', () => {
 
-  let state = "--select state--";
-  let type = "--select type--";
+  let state = "";
+  let type = {
+    micro: false,
+    brewpub: false,
+    contract: false,
+    large: false,
+    regional: false
+  }
 
   function setState(s) {
     state = s;
   }
-  function setType(s) {
-    type = s;
+  function setType(obj) {
+    type = obj;
   }
 
   it('Renders', () => {
-    render(<Filters setStateFilter={setState} setTypeFilter={setType} />);
-    const boxes = screen.getAllByRole('combobox');
-    expect(boxes[0]).toHaveTextContent('--select state--');
-    expect(boxes[1]).toHaveTextContent('--select type--');
-  })
+    render(<Filters setStateFilter={setState} typeFilterState={type} setTypeFilterState={setType} />);
+    expect(screen.getByRole('combobox')).toHaveTextContent('--select state--');
+    const checkboxes = screen.getAllByRole('checkbox');
+    expect(checkboxes[1].id).toBe('brewpub');
+    })
 
   it('Changes both state and type variables', () => {
-    render(<Filters setStateFilter={setState} setTypeFilter={setType} />);
-    const boxes = screen.getAllByRole('combobox');
-    fireEvent.change(boxes[0], {target: {value: "colorado"}});
-    expect(boxes[0].value).toBe('colorado');
+    render(<Filters setStateFilter={setState} typeFilterState={type} setTypeFilterState={setType} />);
+    fireEvent.change(screen.getByRole('combobox'), {target: {value: "colorado"}});
+    expect(screen.getByRole('combobox').value).toBe('colorado');
     expect(state).toBe('colorado');
-    fireEvent.change(boxes[1], {target: {value: "micro"}});
-    expect(boxes[0].value).toBe('colorado');
-    expect(boxes[1].value).toBe('micro');
-    expect(type).toBe('micro');
+    const checkboxes = screen.getAllByRole('checkbox');
+    fireEvent.click(checkboxes[0]);
+    expect(screen.getByRole('combobox').value).toBe('colorado');
+    expect(checkboxes[0].value).toBe('on');
+    expect(type.micro).toBe(true);
+    fireEvent.click(checkboxes[0]);
+    expect(type.micro).toBe(false);
   })
 
 })
