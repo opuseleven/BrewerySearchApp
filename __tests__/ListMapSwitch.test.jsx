d5c30369844b798e33609323bfecbb617731ac5a
Cody Clark <opuseleven@protonmail.com>
2022-03-06 13:58:17 -0600

    Added one more test.

--- __tests__/ListMapSwitch.test.jsx
+++ __tests__/ListMapSwitch.test.jsx
@@ -14,7 +14,14 @@ describe('ListMapSwitch component', () => {
     expect(screen.getByRole('button')).toHaveTextContent("Show Map");
   })
 
+  it('Renders correctly if showMap=true', () => {
+    setShowMap(true);
+    render(<ListMapSwitch showMap={showMap} setShowMap={setShowMap} />);
+    expect(screen.getByRole('button')).toHaveTextContent("Hide Map");
+  })
+
   it('Sets the showMap variable', () => {
+    setShowMap(false);
     render(<ListMapSwitch showMap={showMap} setShowMap={setShowMap} />);
     fireEvent.click(screen.getByRole('button'));
     expect(showMap).toBe(true);
