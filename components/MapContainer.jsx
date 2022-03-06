a99ddc5298100a7f449a9176660927e39e6bce81
Cody Clark <opuseleven@protonmail.com>
2022-03-06 10:46:50 -0600

    Outlined MapContainer component

---
+++ components/MapContainer.jsx
@@ -0,0 +1,26 @@
+import Map, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+function MapContainer({ center }) {
+
+  const accessToken = 'pk.eyJ1Ijoib3B1c2VsZXZlbiIsImEiOiJja3R4Zzlkem0ydHF6MnBvM2p2b211cXpmIn0.wf4TNUb_e9A8namFw9ojQA'
+
+  return (
+    <div>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+          center: center
+          }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={accessToken}
+      >
+        <Marker longitude={center[0]} latitude={center[1]} color="red" />
+      </Map>
+    </div>
+  )
+}
+export { MapContainer };
