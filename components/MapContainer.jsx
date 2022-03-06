ca3ea5989ffb4cbd0e0699adee4849c979c50dfd
Cody Clark <opuseleven@protonmail.com>
2022-03-06 14:57:33 -0600

    Fixed centering of map

--- components/MapContainer.jsx
+++ components/MapContainer.jsx
@@ -5,16 +5,13 @@ function MapContainer({ center }) {
 
   const accessToken = 'pk.eyJ1Ijoib3B1c2VsZXZlbiIsImEiOiJja3R4Zzlkem0ydHF6MnBvM2p2b211cXpmIn0.wf4TNUb_e9A8namFw9ojQA'
 
-  const defaultCenter  = center;
-
   return (
     <div role="map">
       <Map
         initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
+          longitude: center[0],
+          latitude: center[1],
           zoom: 14,
-          center: defaultCenter
           }}
         style={{width: 600, height: 400}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
