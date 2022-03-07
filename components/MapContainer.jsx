b1888209589916ea1e008afd5338ce90069e9c8e
Cody Clark <opuseleven@protonmail.com>
2022-03-07 12:58:11 -0600

    arr parameter is passed to component. Added Markers for arr.

--- components/MapContainer.jsx
+++ components/MapContainer.jsx
@@ -1,23 +1,33 @@
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-function MapContainer({ center }) {
+function MapContainer({ arr }) {
 
   const accessToken = 'pk.eyJ1Ijoib3B1c2VsZXZlbiIsImEiOiJja3R4Zzlkem0ydHF6MnBvM2p2b211cXpmIn0.wf4TNUb_e9A8namFw9ojQA'
+  let defaultCenter = [-86.767960, 36.174465];
+  if (arr) {
+    defaultCenter = [arr[0].longitude, arr[0].latitude];
+  }
 
   return (
     <div role="map">
       <Map
         initialViewState={{
-          longitude: center[0],
-          latitude: center[1],
-          zoom: 14,
+          longitude: defaultCenter[0],
+          latitude: defaultCenter[1],
+          zoom: 8,
           }}
         style={{width: 600, height: 400}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={accessToken}
       >
-        <Marker longitude={center[0]} latitude={center[1]} color="red" />
+        {
+          arr && (
+            arr.map(b => (
+              <Marker longitude={b.longitude} latitude={b.latitude} color="red" />
+            ))
+          )
+        }
       </Map>
     </div>
   )
