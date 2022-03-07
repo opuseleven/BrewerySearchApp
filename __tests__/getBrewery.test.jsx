9af5ad802cb06ef06f28e5e1e7373f3bf8e5e90f
Cody Clark <opuseleven@protonmail.com>
2022-03-07 09:31:38 -0600

    Fixed test for longitude and latitude parameters

--- __tests__/getBrewery.test.jsx
+++ __tests__/getBrewery.test.jsx
@@ -9,11 +9,13 @@ describe('getBrewery function', () => {
     street: "123 Street St.",
     city: "Nashville",
     state: "Tennessee",
-    website_url: "www.duckduckgo.com"
+    website_url: "www.duckduckgo.com",
+    longitude: -86.767960,
+    latitude: 36.174465
   }
 
   it('Creates a new Brewery object and returns that object', () => {
-    const brewery = getBrewery(1, "Test Brewery", "Microbrewery", "123 Street St.", "Nashville", "Tennessee", "www.duckduckgo.com");
+    const brewery = getBrewery(1, "Test Brewery", "Microbrewery", "123 Street St.", "Nashville", "Tennessee", "www.duckduckgo.com", -86.767960, 36.174465);
     expect(brewery).toStrictEqual(expectedBrewery);
   })
 })
