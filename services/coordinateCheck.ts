39455d3bf331263aaa3d87c2e9be5dacd272a5af
Cody Clark <opuseleven@protonmail.com>
2022-03-07 12:57:34 -0600

    Fixed to account for lat & lon = null

--- services/coordinateCheck.ts
+++ services/coordinateCheck.ts
@@ -4,9 +4,13 @@ function coordinateCheck(brewery: Brewery) {
   let check = false;
   if (brewery.latitude !== undefined) {
     if (brewery.longitude !== undefined) {
-      if (brewery.latitude.toString().length > 1) {
-        if (brewery.longitude.toString().length > 1) {
-          check = true;
+      if (brewery.latitude !== null) {
+        if (brewery.longitude !== null) {
+          if (brewery.latitude.toString().length > 1) {
+            if (brewery.longitude.toString().length > 1) {
+              check = true;
+            }
+          }
         }
       }
     }
