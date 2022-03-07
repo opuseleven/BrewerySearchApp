fa20a4bd4d6b20600197fe64b8d6c9e7b43c2eb4
Cody Clark <opuseleven@protonmail.com>
2022-03-07 11:45:22 -0600

    Fixed coordinateCheck function to account for optional parameters

--- services/coordinateCheck.ts
+++ services/coordinateCheck.ts
@@ -2,9 +2,13 @@ import { Brewery } from '../types';
 
 function coordinateCheck(brewery: Brewery) {
   let check = false;
-  if (brewery.longitude.toString().length > 1) {
-    if (brewery.latitude.toString().length > 1) {
-      check = true;
+  if (brewery.latitude !== undefined) {
+    if (brewery.longitude !== undefined) {
+      if (brewery.latitude.toString().length > 1) {
+        if (brewery.longitude.toString().length > 1) {
+          check = true;
+        }
+      }
     }
   }
   return check;
