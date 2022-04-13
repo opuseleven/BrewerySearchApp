0bae061b103f8a0ba596564775f80bd97dbb4ada
Cody Clark <opuseleven@protonmail.com>
2022-04-13 13:05:55 -0500

    Added env var for ACCESSTOKEN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config();
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ACCESSTOKEN: process.env.ACCESSTOKEN,
+  },
 }
 
 module.exports = nextConfig
