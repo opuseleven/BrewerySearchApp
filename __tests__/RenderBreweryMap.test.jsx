dc4c95802600e5b76c0e6194569736c035b633ae
Cody Clark <opuseleven@protonmail.com>
2022-04-19 18:37:51 -0500

    Fixed test

--- __tests__/RenderBreweryMap.test.jsx
+++ __tests__/RenderBreweryMap.test.jsx
@@ -1,5 +1,6 @@
 import { RenderBreweryMap } from '../components';
 import { getBrewery } from '../services';
+import { Map } from 'react-map-gl';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
@@ -8,6 +9,19 @@ describe('RenderBreweryMap Component', () => {
   const testBrewery = getBrewery(1, "Test Brewery", "Brewpub", "123 Main st", "Nashville", "Tennessee", "www.duckduckgo.com", -86.767960, 36.174465);
 
   it('Component renders', () => {
-    render(<RenderBreweryMap brewery={testBrewery} />);
+    render(
+      <Map
+        initialViewState={{
+          longitude: testBrewery.longitude,
+          latitude: testBrewery.latitude,
+          zoom: 8
+        }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.ACCESSTOKEN}
+      >
+        <RenderBreweryMap brewery={testBrewery} />
+      </Map>
+      );
   })
 })
