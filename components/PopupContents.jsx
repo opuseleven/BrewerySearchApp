a124c647d7e8fdd59ae053bd59bab699bbdadc75
Cody Clark <opuseleven@protonmail.com>
2022-05-28 14:05:55 -0500

    Added PopupContents component

---
+++ components/PopupContents.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Components.module.css';
+
+function PopupContents({ brewery }) {
+  return (
+    <div className={styles.popupcontents}>
+      <h4>{brewery.name}</h4>
+      <ul>
+        <li>Type: {brewery.brewery_type}</li>
+        <li><a href={brewery.website_url} target="_blank">{brewery.website_url}</a></li>
+      </ul>
+      <p>{brewery.street}</p>
+      <p>{brewery.city}, {brewery.state}</p>
+    </div>
+  )
+}
+export { PopupContents };
