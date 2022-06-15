8d8e075690bf3fd96309037898377fa830503e05
Cody Clark <opuseleven@protonmail.com>
2022-06-15 17:40:57 -0500

    Fixed test for MapContainer

--- __tests__/MapContainer.test.jsx
+++ __tests__/MapContainer.test.jsx
@@ -7,8 +7,14 @@ describe('MapContainer component', () => {
   const testData = require('../data/breweries.json');
   const arr = filterByHasCoordinates(testData);
 
+  let selectedBrewery;
+  function setSelectedBrewery(b) {
+    selectedBrewery = b;
+  }
+
   it('Component renders', () => {
-    render(<MapContainer arr={arr} />);
+    render(<MapContainer arr={arr} selectedBrewery={selectedBrewery}
+      setSelectedBrewery={setSelectedBrewery} />);
     const map = screen.getByRole('map');
     expect(map).toBeDefined();
   })
