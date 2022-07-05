b7574ecf734031088566360e0c2fb1b2b79f8480
Cody Clark <opuseleven@protonmail.com>
2022-07-05 11:05:48 -0500

    Added defaultTypeFilterState export

--- types/TypeFilterState.ts
+++ types/TypeFilterState.ts
@@ -6,3 +6,11 @@ export interface TypeFilterState {
   large: boolean,
   regional: boolean
 }
+
+export const defaultTypeFilterState: TypeFilterState = {
+  micro: false,
+  brewpub: false,
+  contract: false,
+  large: false,
+  regional: false
+}
