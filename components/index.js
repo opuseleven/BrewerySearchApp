701df9d77fd999827880c8c0440771e69ba0cf00
Cody Clark <opuseleven@protonmail.com>
2022-07-05 10:53:53 -0500

    Removed old export

--- components/index.js
+++ components/index.js
@@ -2,7 +2,6 @@ export * from './SearchForm';
 export * from './RenderBrewery';
 export * from './Filters';
 export * from './StateSelector';
-export * from './BreweryTypeSelector';
 export * from './BreweryTypeFilters';
 export * from './ListMapSwitch';
 export * from './MapContainer';
