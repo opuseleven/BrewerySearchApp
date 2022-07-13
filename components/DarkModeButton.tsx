6772ee4e1bb00e6078c7f3d56588c17b96a86084
Cody Clark <opuseleven@protonmail.com>
2022-07-12 20:02:08 -0500

    Outlined DarkModeButton component

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -1,7 +1,23 @@
-const DarkModeButton: React.FC = () => {
+import styles from '../styles/Components.module.css';
+
+interface DarkModeButtonProps {
+  darkMode: boolean,
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const DarkModeButton: React.FC<DarkModeButtonProps> = ({ darkMode, setDarkMode }) => {
+
+  function handleClick() {
+    setDarkMode(!darkMode);
+  }
 
   return (
     <div>
+      <button className={styles.darkmodebutton}
+        onClick={() => handleClick()}
+      >
+        {'\u263e'}
+      </button>
     </div>
   )
 }
