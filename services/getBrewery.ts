6df4e918c8cb9eb9090bf5cdc1d12e4cc83124db
Cody Clark <opuseleven@protonmail.com>
2022-11-01 17:02:45 -0500

    Cleaned up formatting

--- services/getBrewery.ts
+++ services/getBrewery.ts
@@ -2,7 +2,10 @@ import { Brewery } from '../types';
 
 // Takes parameters and returns a Brewery Object
 
-function getBrewery(id: number, name: string, type: string, street: string, city: string, state: string, website: string, longitude: number, latitude: number) {
+function getBrewery(id: number, name: string, type: string, street: string,
+  city: string, state: string, website: string, longitude: number,
+  latitude: number) {
+    
   const newBrewery: Brewery = {
     obdb_id: id,
     name: name,
